af972cae56c2028c6ec6c420e5fac42f5e2534db
MaxSNava <max.s015@outlook.com>
2024-09-24 10:51:22 -0600

    feat(seed): end endpoint to populate MongoDB

--- README.md
+++ README.md
@@ -19,6 +19,10 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Poblar la base de datos usando una semilla
+```
+localhost:3000/api/v2/seed
+```
 
 ## Stack usado
 * MongoDB
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-
-  private readonly axios: AxiosInstance = axios;
+  //! __ CONSTURCTOR __
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
+  ){}
 
   async executeSeed() {
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    await this.pokemonModel.deleteMany({}); // Clean the collection
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=151')
+    const pokemonToInsert: {name: string, no:number}[] = [];
     data.results.forEach(({name, url}) => {
       const segments = url.split('/');
-      const num:number = +segments[segments.length - 2];
-      // console.log(`Name: ${name}, Number: ${num}`)
-    })
-    return data.results;
+      const no = +segments[segments.length - 2];
+      // const pokemon = await this.pokemonModel.create({name, no});
+      pokemonToInsert.push({name, no});
+    });
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return 'Seed executed';
   }
 
 }
